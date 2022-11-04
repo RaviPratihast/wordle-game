4c2ba03d203e7ad6fd600a170e6a2ab12f2baae9
RAVI SHANKAR PRATIHAST <75324945+RaviPratihast@users.noreply.github.com>
2022-11-04 21:13:18 +0530

    UI: Header and title done.

--- src/wordleApp.js
+++ src/wordleApp.js
@@ -1,8 +1,19 @@
 import React from "react";
+import "./wordleApp.css"
 
 function App(){
     return <div className="game-container">
-     
+     {/* header part */}
+      <header>
+        <h1>Wordle: Game of Words</h1>
+      </header>
+
+      {/* screen for the words to appear */}
+
+
+      {/* buttons or can say keyboard */}
+
+      {/* footer: year of making and some copyright text */}
     </div>
 }
 
